3f135443fcd1cb62b1ba2b37cec03236f0fc8c8c
Agung Priyatno <agungpriyatno303@gmail.com>
2026-04-23 10:39:56 +0700

    fix: db connection leak and exports

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,17 +1,21 @@
-import { PrismaPg } from "@prisma/adapter-pg";
-import { Pool } from "pg";
 import { PrismaClient } from "@/generated/prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
 
-// Mengambil DATABASE_URL dari environment
 const connectionString = `${process.env.DATABASE_URL}`;
 
-// Membuat pool koneksi menggunakan pg
-const pool = new Pool({ connectionString });
+const prismaClientSingleton = () => {
+  const adapter = new PrismaPg({ connectionString });
+  return new PrismaClient({ adapter });
+};
 
-// Membuat instance dari PrismaPg adapter
-const adapter = new PrismaPg(pool);
+type PrismaClientSingleton = ReturnType<typeof prismaClientSingleton>;
 
-// Inisialisasi Prisma Client dengan adapter
-const prisma = new PrismaClient({ adapter });
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClientSingleton | undefined;
+};
 
-export default prisma;
+const prisma = globalForPrisma.prisma ?? prismaClientSingleton();
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+
+export { prisma };
